f5ff6b404feddbba9be22d3b0ff2e621d53831f9
jishnuperiya <jishnuperiya@gmail.com>
2023-05-23 17:45:13 +0200

    function template modified to take diffetent types of parameters

--- 3) Miscallaneous/function_template.cpp
+++ 3) Miscallaneous/function_template.cpp
@@ -5,6 +5,16 @@ T add(T a, T b){
     return a+b;
 }
 
+
+// Question: what if a and b are of different types?
+// To handle addition of values of different types, you would need to overload the function for specific types
+
+template <typename T, typename U>
+auto add(T a, U b) {
+    return a + b;
+}
+
+
 int main(){
 int a= 10;
 int b=7;
@@ -12,7 +22,14 @@ int b=7;
 double c=12;
 double d=7;
 
+int e=12;
+float f=0.50;
+
 std::cout << add(a,b) << std::endl;
 std::cout << add(c,d) << std::endl;
+std::cout << add(e,f) << std::endl;
+
+
+
 
 }
